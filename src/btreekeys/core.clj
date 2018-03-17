5cd418094e62308f0898dc015f43795c658bdf31
Patrik Karlin <patrik.karlin@gmail.com>
2018-03-17 17:14:06 +0000

    initial commit.
    Some functions and tests

---
+++ src/btreekeys/core.clj
@@ -0,0 +1,170 @@
+(ns btreekeys.core
+  ""
+  (:import java.nio.ByteBuffer
+           java.util.Arrays))
+
+(defmulti key-structure
+  "overide to add a new keystructure. "
+  identity)
+
+(defmulti keysegment-size
+  "returns the byte size of key part type"
+  identity)
+
+(defmulti keysegment-write-code
+  "returns the code to write the keypart to the bite buffer"
+  (fn [keypart-type
+      buffer-expr
+      value-expr] keypart-type))
+
+(defmulti keysegment-read-code
+  "returns the code to reads the keypart from a bite buffer"
+  (fn [keypart-type
+      buffer-expr] keypart-type))
+
+(defn- assert-structure-variances
+  [structure-type]
+  (when-let [key (->> (key-structure structure-type)
+                      (map first)
+                      (frequencies)
+                      (filter (fn [[k c]] (> c 1)))
+                      (first))]
+    (throw (ex-info "key appeared more then once in key-structure"
+                    {:structure-type structure-type :key key}))))
+
+(defn keysegment-type
+  [structure-type keysegment-key]
+  (->> (key-structure structure-type)
+       (some (fn [[k t]] (when (= k keysegment-key) t)))))
+
+(defn- assert-key-in-structure
+  [structure-type keysegment-key]
+  (when-not (keysegment-type structure-type keysegment-key)
+    (throw (ex-info "keysegment-key does not appear in key-structure"
+                    {:structure-type structure-type
+                     :keysegment-key keysegment-key}))))
+
+(defn keysegment-range
+  [structure-type keysegment-key]
+  (let [structure (key-structure structure-type)
+        before-keysegment (take-while
+                            (fn [[k t]] (not= k keysegment-key))
+                            structure)
+        size-before-keysegment
+        (reduce + (map (comp keysegment-size second)
+                       before-keysegment))]
+    [size-before-keysegment
+     (dec
+       (+ size-before-keysegment
+          (keysegment-size
+            (keysegment-type structure-type keysegment-key))))]))
+
+(defn key-size
+  [structure-type]
+  (reduce
+    +
+    (map (comp keysegment-size second)
+         (key-structure structure-type))))
+
+(defn- make-key-code
+  [structure prefix-expressions]
+  (let [key-size (->> structure
+                      (map (comp keysegment-size second))
+                      (reduce +))
+        buffer-sym (gensym "buffer")]
+    `(let [~buffer-sym (ByteBuffer/allocate ~key-size)]
+       ~@(for [[keysegment-key keysegment-type] structure]
+           (keysegment-write-code
+             keysegment-type
+             buffer-sym
+             (get prefix-expressions keysegment-key)))
+       (.array ~buffer-sym))))
+
+(defmacro make-key
+  [structure-type input-map-expr]
+  (assert-structure-variances structure-type)
+  (let [input-map-sym (gensym "input-map")
+        structure (key-structure structure-type)]
+    `(let [~input-map-sym ~input-map-expr]
+       ~(make-key-code
+          structure
+          (into {} (for [[k _] structure]
+                     [k `(get ~input-map-sym ~k)]))))))
+
+(defn- assert-is-valid-prefix
+  [_ _]
+  ;; TODO
+ )
+
+(defn- prefix-structure
+  [structure-type prefix-keys]
+  (let [prefix-set (set prefix-keys)]
+    (take-while
+      #(prefix-set (first %))
+      (key-structure structure-type))))
+
+(defmacro make-key-prefix
+  [structure-type & {:as prefix-expressions}]
+  (assert-structure-variances structure-type)
+  (assert-is-valid-prefix structure-type (keys prefix-expressions))
+  (make-key-code
+    (prefix-structure structure-type (keys prefix-expressions))
+    prefix-expressions))
+
+(defn- parse-code
+  [structure input-bytes-expr]
+  (let [buffer-sym (gensym "buffer")]
+    `(let [~buffer-sym (ByteBuffer/wrap ~input-bytes-expr)]
+       (into
+         {}
+         [~@(for [[keysegment-key keysegment-type] structure]
+              `[~keysegment-key
+                ~(keysegment-read-code
+                   keysegment-type
+                   buffer-sym)])]))))
+
+(defmacro parse-key
+  [structure-type input-bytes-expr]
+  (assert-structure-variances structure-type)
+  (parse-code
+    (key-structure structure-type)
+    input-bytes-expr))
+
+(defmacro parse-key-prefix
+  [structure-type prefix-keys input-bytes-expr]
+  (assert-structure-variances structure-type)
+  (assert-is-valid-prefix structure-type prefix-keys)
+  (parse-code
+    (prefix-structure structure-type prefix-keys)
+    input-bytes-expr))
+
+(defn increment-byte-range!
+  [^bytes input-array ^long range-start ^long range-end]
+  (loop [idx range-end]
+    (when (>= idx range-start)
+      (let [val (aget input-array idx)]
+        (if (= 127 val)
+          (recur (dec idx))
+          (do (aset-byte input-array idx (+ val 1))
+              true))))))
+
+(defmacro increment-key-segment!
+  [structure-type keysegment-key bytes-expr]
+  (assert-structure-variances structure-type)
+  (assert-key-in-structure structure-type keysegment-key)
+  (let [[start end] (keysegment-range structure-type keysegment-key)]
+    `(increment-byte-range! ~bytes-expr ~start ~end)))
+
+;; =============================================================================
+;; built in keysegment types
+
+(defmethod keysegment-size :long
+  [_] Long/BYTES)
+
+(defmethod keysegment-write-code :long
+  [_ buffer-expr long-expr]
+  `(.putLong ^ByteBuffer ~buffer-expr ~long-expr))
+
+(defmethod keysegment-read-code :long
+  [_ buffer-expr]
+  `(.getLong ^ByteBuffer ~buffer-expr))
---
+++ test/btreekeys/core_test.clj
@@ -0,0 +1,66 @@
+(ns btreekeys.core-test
+  (:require [clojure.test :refer :all]
+            [btreekeys.core :as bt]))
+
+(defmethod key-structure ::sample
+  [_]
+  [[:head :long]
+   [:body :long]
+   [:tail :long]])
+
+(deftest test-make-key
+  (is (= (seq (bt/make-key ::sample
+                           {:head 123
+                            :body 321
+                            :tail 765}))
+         [0, 0, 0, 0, 0, 0, 0, 123, 0, 0, 0, 0,
+          0, 0, 1, 65, 0, 0, 0, 0, 0, 0, 2, -3])))
+
+(deftest test-parse-key
+  (let [original {:head 123 :body 321 :tail 765}
+        k (bt/make-key ::sample original)]
+    (is (= (bt/parse-key ::sample k) original))))
+
+(deftest test-make-prefix
+  (is (= (seq (bt/make-key-prefix
+                ::sample
+                :head 231213112313
+                :body 432432434323))
+         [0, 0, 0, 53, -43, 96, 27, -7, 0, 0, 0,
+          100, -82, -5, 76, -109])))
+
+(deftest keysegment-range-test
+  (are [k r] (= r (keysegment-range ::sample k))
+    :head [0 7]
+    :body [8 15]
+    :tail [16 23]))
+
+(deftest test-parse-prefix
+  (is (= (bt/parse-key-prefix
+           ::sample
+           [:head :body]
+           (bt/make-key-prefix
+             ::sample
+             :head 231213112313
+             :body 432432434323))
+         {:head 231213112313
+          :body 432432434323})))
+
+(deftest increment-key-segment-test
+  (let [key (bt/make-key
+              ::sample
+              {:head 123 :body 122 :tail 123})]
+    (is (bt/increment-key-segment!
+          ::sample :body key))
+    (is (= (seq key)
+           [0 0 0 0 0 0 0 123
+            0 0 0 0 0 0 0 123
+            0 0 0 0 0 0 0 123])))
+  (let [max-key-source
+        [0 0 0 0 0 0 0 1
+         127 127 127 127 127 127 127 127
+         0 0 0 0 0 0 0 1]
+        max-key (byte-array max-key-source)]
+    (is (not (bt/increment-key-segment!
+               ::sample :body max-key)))
+    (is (= (seq max-key) max-key-source))))
