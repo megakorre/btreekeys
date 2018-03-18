3bcd0a1f7d0329291acbfbe4fefda12557d3158e
Patrik Karlin <patrik.karlin@gmail.com>
2018-03-18 22:10:03 +0000

    bump version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject megakorre/btreekeys "0.1.0"
+(defproject megakorre/btreekeys "0.1.1"
   :description "A library for creating parsing and traversing btrees effectively"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
